f82c3c0adcc9aa027ffcc618e5da5b264e0d3552
demijevu <demijevu@gmail.com>
2023-04-23 11:38:11 +0800

    corrected

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,102 +1,136 @@
+console.log("logic.js");
+
+let basemap = L.tileLayer(
+    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
+    {
+        attribution:
+            'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    });
+
+let themap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+let mymap = L.map("map", {
+    center: [
+        40.7, -94.5
+    ],
+    zoom: 3,
+});
+
+basemap.addTo(mymap);
+
+let baseMaps = {
+    "Global Earthquakes": basemap,
+    "Global Blue": themap,
+};
+
+let tectonicplates = new L.LayerGroup();
+let earthquakes = new L.LayerGroup();
+
+let overlays = {
+    "Tectonic Plates": tectonicplates,
+    Earthquakes: earthquakes
+};
+
+L.control
+    .layers(baseMaps, overlays, { collapsed: false })
+    .addTo(mymap);
+
 
 let baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a get request to the baseURL send the features once we get a response
 d3.json(baseURL).then(function (data) {
-    createFeatures(data.features);
-});
+    function styleInfo(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
 
-// Define a function that we want to run once for each feature in the features array.
-// Give each feature a popup that describes the place and time of the earthquake.
+    function getColor(depth) {
+        switch (true) {
+            case depth > 90:
+                return "#ea2c2c";
+            case depth > 70:
+                return "#ea822c";
+            case depth > 50:
+                return "#ee9c00";
+            case depth > 30:
+                return "#eecc00";
+            case depth > 10:
+                return "#d4ee00";
+            default:
+                return "#98ee00";
+        }
+    }
 
-function createFeatures(earthquakeData) {
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+
+        return magnitude * 4;
+    }
+
     function onEachFeature(feature, layer) {
         layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}</p><hr><p>Number of "Felt" Reports: ${feature.properties.felt}`);
-
-        // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-        // Run the onEachFeature function once for each piece of data in the array.
-
-
-        let earthquakes = L.geoJSON(earthquakeData, {
-            onEachFeature: onEachFeature,
-            pointTolayer: createCircleMarker
-        });
-
-
-        // Create a GeoJSON layer containing the features array on the earthquakeData object
-        function createCircleMarker(feature, latlng) {
-            let options = {
-                radius: feature.properties.mag * 5,
-                fillColor: chooseColor(feature.properties.mag),
-                color: chooseColor(feature.properties.mag),
-                weight: 1,
-                opacity: 0.8,
-                fillOpacity: 0.35
-            }
-            return L.circleMarker(latlng, options);
-            //    create Map Functions   
-            createMap(earthquakes);
-
-            
-// Loop through the cities array, and create one marker for each city object.
-for (let i = 0; i < feature.properties.mag; i++) {
-
-    // Conditionals for country gdp_pc
-    let color = "";
-    if (feature.properties.mag <= 2.5) {
-      color = "yellow";
-    }
-    else if (feature.properties.mag <= 4) {
-      color = "blue";
-    }
-    else if (feature.properties.mag <= 5.5) {
-      color = "red";
-    }
-    else if (feature.properties.mag <= 8) {
-        color = "pink";
-    }
-    else if (feature.properties.mag <= 20) {
-        color = "orange";
-    }
-    else {
-      color = "violet";
     }
 
-            // function chooseColor(mag) {
-            //     switch (true) {
-            //         case (1.0 <= mag && mag <= 2.5):
-            //             return "#0071BC"; // Strong blue
-            //         case (2.5 <= mag && mag <= 4.0):
-            //             return "#35BC00";
-            //         case (4.0 <= mag && mag <= 5.5):
-            //             return "#BCBC00";
-            //         case (5.5 <= mag && mag <= 8.0):
-            //             return "#BC3500";
-            //         case (8.0 <= mag && mag <= 20.0):
-            //             return "#BC0000";
-            //         default:
-            //             return "#E2FFAE";
-            //     }
-            // }
+    L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
 
-            // create map legend
-            let legend = L.control({ position: 'bottomright' });
-            legend.onAdd = function () {
-                let div = L.DomUtil.create('div', 'info legend');
-                let grades = [1.0, 2.5, 4.0, 5.5, 8.0];
-                let labels = [];
-                let legendInfo = "<h4>Magnitude</h4>";
+        onEachFeature: onEachFeature,
+    }).addTo(earthquakes);
 
-                legendInfo = div.innerHTML
+    earthquakes.addTo(mymap);
 
-                // go through each magnitude item to label and color the legend
-                // push to labels array as list item
-                for (let i = 0; i < grades.length; i++) {
-                    labels.push('<ul style="background-color:' + chooseColor(grades[i] + 1) + '"> <span>' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '' : '+') + '</span></ul>');
-                }
+    let legend = L.control({
+        position: "bottomright"
+    });
 
-                // add each label list item to the div under the <ul> tag
-                div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend");
 
-                return div;
-            };
+        let grades = [-10, 10, 30, 50, 70, 90];
+
+        let colors = [
+            "#98ee00",
+            "#d4ee00",
+            "#eecc00",
+            "#ee9c00",
+            "#ea822c",
+            "#ea2c2c"];
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML += "<i style='background: "
+                + colors[i]
+                + "'></i> "
+                + grades[i]
+                + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        }
+        return div;
+    };
+
+    legend.addTo(mymap);
+
+    d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (platedata) {
+
+        L.geoJson(platedata, {
+            color: "orange",
+            weight: 2
+        }).addTo(tectonicplates);
+
+        tectonicplates.addTo(mymap);
+    });
+
+})
